b34098bdfdaa0c133228338df221e296ae53d425
Momfus <momfus@outlook.com>
2020-08-09 16:06:12 -0300

    añadido las rutas y componentes bases - clase 211

---
+++ src/app/@core/services/api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor( private apollo: Apollo) { }
+
+  // Lista de los personajes de Breaking Bad
+  getCharacters() {
+
+
+
+  }
+
+}
